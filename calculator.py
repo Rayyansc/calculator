1ba9fc005e0d097b7eac711b5cb37f4a85155973
Rayyan <rayyancheema@outlook.com>
2026-07-30 09:44:23 +0100

    Quiz Questions to change code

--- calculator.py
+++ calculator.py
@@ -1,15 +1,11 @@
+print("Welcome to Quantome Quiz")
 print('Enter your name:')
 x = input()
 print('Hello, ' + x)
-print("Please enter your first digit")
-y = input
-print("Your first digit was," + y)
-print("Enter your operator")
-z= input
-print("your operator was," + z)
-print("Please enter your last digit")
-a = input
-print("Your first digit was" + a)
-y,z,a = input
-def func():
-    print("y z a")
+Answer = input("Do you want to continue?  (yes/no): ")
+if Answer == "yes":
+    print("Great! Let's start the quiz.") #Go straight to line 10
+else:
+    print("Thank you for cosidering us today. We hope to see you again soon!") #Do not go to line 10
+print("Question 1: What is the capital of Pakistan?")
+Answer1 = input("a) Islamabad\nb) Karachi\nc) Lahore\nd) Peshawar\nYour answer:") 
